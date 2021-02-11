6e32f8eee0384f675cd1004e280d899aeaa1a2da
rohitsainier <rohitsainier@gmail.com>
2021-02-11 10:55:15 +0530

    Initial Commit

---
+++ RizzleClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RizzleClone
+//
+//  Created by Rohit Saini on 11/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
